3ca9b4f1ab0b26fd2c5bfb14c853ab1dd99c5c03
ZhenXin <Chin.Zhen.Xin@student.mmu.edu.my>
2026-05-28 18:24:17 +0800

    Part A - Main Structure, Login, Register, Logout, Main Menu

--- Project.cpp
+++ Project.cpp
@@ -1,7 +1,276 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <cstring>
 using namespace std;
 
+struct Book {
+    string bookID;
+    string title;
+    string author;
+    string category;
+    int year;
+    int stock;
+    string status; // "Available" or "Borrowed"
+};
+
+struct BookRecord {
+    string borrowID;
+    string customerID;
+    string bookID;
+    string borrowDate;
+    string returnDate;
+    string borrowStatus; // "Borrowed" or "Returned"
+};
+
+class Account {
+    protected:
+        string username;
+        string password;
+        string role; // "Customer" or "Librarian"
+    
+    public:
+        Account(){
+            username = "";
+            password = "";
+            role = "";
+        }
+
+        ~Account(){} // Destructor (Run when the object is destroyed)
+
+        friend void displayAccount(const Account& a);
+        friend class Admin;
+        
+        string getUsername(){
+            return username;
+        }
+
+        string getPassword(){
+            return password;
+        }
+
+        string getRole(){
+            return role;
+        }
+};
+
+class User : public Account {
+    public :
+        User() {
+            role = "Customer";
+        }
+
+        User(string u, string p) {
+            username = u;
+            password = p;
+            role = "Customer";
+        }
+
+        ~User(){}
+        friend void displayUser(const User& u);
+
+        void setUsername(string u){
+            username = u;
+        }
+
+        void setPassword(string p){
+            password = p;
+        }
+};
+
+class Admin : public Account {
+    public:
+        Admin(){
+            role = "Admin";
+        }
+
+        Admin(string u, string p){
+            username = u;
+            password = p;
+            role = "Admin";
+        }
+        
+        ~Admin(){}
+        friend void displayAdmin(const Admin& a);
+
+        void setUsername(string u){
+            username = u;
+        }
+
+        void setPassword(string p){
+            password = p;
+        }
+};
+
+void registerUser(){
+    string u, p;
+    cout << "\n== Register New Customer ==" << endl;
+    cout << "Enter username: ";
+    cin >> u;
+    cout << endl << "Enter password: ";
+    cin >> p;
+
+    // Dynamically allocate a new User object
+    User* newUser = new User(u, p); // Create a new User with username and password
+    ofstream file("Customer.txt", ios::app);
+    file << newUser->getUsername() << "|" << newUser->getPassword() << "|Customer" << endl;
+    file.close();
+    delete newUser; // Free memory after use
+
+    cout << "Customer registered successfully!" << endl;
+}
+
+string loginUser(){
+    string u, p, fu,fp, frole; // fu = file username, fp = file password, frole = file role
+    cout << "\n == Customer Login ==" << endl;
+    cout << "Enter username: ";
+    cin >> u;
+    cout << endl << "Enter password: ";
+    cin >> p;
+
+    ifstream file("Customer.txt");
+    // getline(自己取的file name, 读出来的资料存去哪个变量, 遇到什么stop)
+    while (getline(file,fu,'|') && getline(file, fp, '|') && getline(file, frole)){
+        if(u == fu && p == fp)
+        {
+            file.close();
+            cout << "Login successful! Welcome, " << u << "!" << endl;
+            return u; // Return the username of the logged in user
+        }
+    }
+
+    file.close();
+    cout << "Invalid username or password." << endl;
+    return "";
+}
+
+string loginAdmin(){
+    string u, p, fu, fp, frole;
+    cout << "\n== Admin Login ==" << endl;
+    cout << "Enter username: ";
+    cin >> u;
+    cout << endl << "Enter password: ";
+    cin >> p;
+
+    ifstream file("Admin.txt");
+    while(getline(file, fu,'|') && getline(file, fp, '|') && getline(file, frole)){
+        if(u == fu && p == fp)
+        {
+            file.close();
+            cout << "Login successful! Welcome, Admin " << u << "!" << endl;
+            return u;
+        }
+    }
+
+    file.close();
+    cout << "Invalid username or password." << endl;
+    return "";
+}
+
+void logout(){
+    cout << "\nLogging out... Goodbye!" << endl;
+}
+
+void customerMenu(string username){
+    int choice;
+    do{
+        cout << "\n=== Customer Menu ===" << endl;
+        cout << "1. Borrow Book" << endl; // Wen Zhe part
+        cout << "2. Return Book" << endl; // Wen Zhe part
+        cout << "3. Search Book" << endl; // Zhong Bao part
+        cout << "4. Display Books" << endl; // Yvonne part
+        cout << "5. View Borrowing Summary" << endl; // Tsui Hern part
+        cout << "0. Logout" << endl;
+        cout << "Enter your choice: ";
+        cin >> choice;
+    } while(choice != 0);
+
+    logout();
+}
+
+void adminMenu(string username){
+    int choice;
+    do{
+        cout << "\n=== Admin Menu ===" << endl;
+        cout << "1. Add Book" << endl; // Yvonne part
+        cout << "2. Edit Book" << endl;// Yvonne part
+        cout << "3. Delete Book" << endl;// Yvonne part
+        cout << "4. Search Book" << endl; // Zhong Bao part
+        cout << "5. Sort Books" << endl; // Zhong Bao part
+        cout << "6. Display Books" << endl; // Yvonne part
+        cout << "7. Generate Reports" << endl; // Tsui Hern part
+        cout << "0. Logout" << endl;
+        cout << "Enter your choice: ";
+        cin >> choice;
+    } while(choice != 0);
+
+    logout();
+}
+
 int main() {
-    cout << "Test" << endl;
+    int choice;
+    do {
+        cout << endl << "=================================";
+        cout << endl << "=  Library Book Records System  =";
+        cout << endl << "=================================" << endl;
+        cout << "1. Customer Login" << endl;
+        cout << "2. Customer Register" << endl;
+        cout << "3. Admin Login" << endl;
+        cout << "0. Exit" << endl;
+        cout << "Enter choice: ";
+        cin >> choice;
+
+        switch(choice){
+            case 1: {
+                try{ // Try to login, if failed throw error
+                    string loggedIn = loginUser(); 
+                    
+                    if(loggedIn.empty())
+                    {
+                        throw "Login failed!"; // Throw error if login failed
+                    }
+                    customerMenu(loggedIn); // Enter customer menu if login successful
+
+                } catch(const char* e){
+                    // Catch the error and display the error message
+                    cout << "Error: " << e << endl;
+                }
+                
+                break;
+            }
+
+            case 2:{
+                registerUser();
+                break;
+            }
+
+            case 3:{
+                try{
+                    string loggedIn = loginAdmin();
+                    
+                    if(loggedIn.empty())
+                    {
+                        throw "Login failed!";
+                    }
+                    adminMenu(loggedIn);
+                } catch(const char* e){
+                    cout << "Error: " << e << endl; 
+                }
+
+                break;
+            }
+
+            case 0:{
+                logout();
+                break;
+            }
+
+            default:{
+                cout << "Invalid choice. Please try again." << endl;
+                break;
+            }
+        }
+    } while(choice != 0);
+
     return 0;
 }
